5afb4d8e1ea9b4431d3078d0118cb32c33edef47
dishadutta <duttadisha12345@gmail.com>
2020-08-06 00:12:04 +0530

    Sort

--- src/post/Posts.js
+++ src/post/Posts.js
@@ -7,18 +7,33 @@ class Posts extends Component {
   constructor() {
     super();
     this.state = {
-      posts: []
+      posts: [],
+      page: 1
     }
   }
 
-  componentDidMount() = () => {
-    list().then(data => {
+  loadPosts = page => {
+    list(page).then(data => {
       if(data.error) {
         console.log(data.error);
       } else {
         this.setState({ posts: data });
       }
-    })
+    });
+  }
+
+  componentDidMount() {
+    this.loadPosts(this.state.page);
+  }
+
+  loadMore = number => {
+    this.setState({page: this.state.page + number});
+    this.loadPosts(this.state.page + number);
+  }
+
+  loadLess = number => {
+    this.setState({page: this.state.page - number});
+    this.loadPosts(this.state.page - number);
   }
 
   renderPosts = (posts) => {
@@ -55,11 +70,25 @@ class Posts extends Component {
   };
 
   render() {
-    const { posts } = this.state;
+    const { posts, page } = this.state;
     return (
       <div className="container">
-        <h2 className="mt-5 mb-5">{!posts.length ? "Loading ... " : "Posts"}</h2>
+        <h2 className="mt-5 mb-5">{!posts.length ? "No More Posts! " : "Posts"}</h2>
         {this.renderPosts(posts)}
+        {
+          page > 1 ? (
+            <button className="btn btn-raised btn-warning mr-5 mt-5 mb-5" onClick={()=>this.loadLess(1)}>
+              Previous ({this.state.page - 1})
+            </button>
+          ) : ( "" )
+        }
+        {
+          posts.length ? (
+            <button className="btn btn-raised btn-warning mr-5 mt-5 mb-5" onClick={()=>this.loadMore(1)}>
+              Next ({this.state.page + 1})
+            </button>
+          ) : ( "" )
+        }
       </div>
     );
   }
